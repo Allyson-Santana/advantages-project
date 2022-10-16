0e2b79a472a713edee9140dc9d1c6b717f540abd
Allyson-Santana <santana.allyson459@gmail.com>
2022-10-16 14:57:03 -0300

    refactor: move Server error to a helper method

--- src/presetation/controllers/singup.ts
+++ src/presetation/controllers/singup.ts
@@ -1,7 +1,6 @@
 import { IvalidParamError } from './errors/invalid-param-error'
 import { MissingParamError } from './errors/missing-param-error'
-import { ServerError } from './errors/server-error'
-import { badRequest } from './helpers/http-helper'
+import { badRequest, serverError } from './helpers/http-helper'
 import { Controller } from './protocols/controller'
 import { EmailValidator } from './protocols/emailValidator'
 import { httpRequest, httpResponse } from './protocols/http'
@@ -27,10 +26,7 @@ export class SingUpController implements Controller {
         return badRequest(new IvalidParamError('email'))
       }
     } catch (error) {
-      return {
-        statusCode: 500,
-        body: new ServerError()
-      }
+      return serverError()
     }
   }
 }
