d314ec07f509e3fb5ff5db8d59445e47b83d5a06
Allyson-Santana <santana.allyson459@gmail.com>
2022-10-16 11:33:54 -0300

    test: ensure SingUpController return an error is no name and email is provided

--- src/presetation/controllers/singup.spec.ts
+++ src/presetation/controllers/singup.spec.ts
@@ -14,5 +14,24 @@ describe('SingUp Controller', () => {
 
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+  })
+})
+
+describe('SingUp Controller', () => {
+  test('should return 400 if email no is provided', () => {
+    const sut = new SingUpController()
+
+    const httpRequest = {
+      body: {
+        name: 'my name',
+        passowrd: 'my passowrd',
+        passowrdConfirmation: 'my passowrdConfirmation'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: e-mail'))
   })
 })
