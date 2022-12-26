8ce492ad8b0b03542ec2a637ace0bebd3c62996e
Allyson-Santana <santana.allyson459@gmail.com>
2022-12-26 20:09:04 -0300

    feat: ensure cors is enable

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,8 @@
 import { Express } from 'express'
 import { boryParser } from '../middlewares/bory-parser'
+import { cors } from '../middlewares/cors'
 
 export default (app: Express): void => {
   app.use(boryParser)
+  app.use(cors)
 }
