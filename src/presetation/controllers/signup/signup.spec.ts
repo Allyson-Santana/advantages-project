9780a2c5be3b6ab86401d415946223cc397323f4
Allyson-Santana <santana.allyson459@gmail.com>
2022-12-30 14:47:24 -0300

    test: change statusCode for created user

--- src/presetation/controllers/signup/signup.spec.ts
+++ src/presetation/controllers/signup/signup.spec.ts
@@ -244,7 +244,7 @@ describe('SignUp Controller', () => {
     }
 
     const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.statusCode).toBe(201)
     expect(httpResponse.body).toEqual({
       id: 'valid_my_id',
       name: 'valid_my_name',
